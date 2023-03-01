94d79e26af5f25e37f849b866189cc83a00e6acb
Aun-Raza <aunrazaq123@gmail.com>
2023-03-01 17:11:45 -0500

    Closes #1; Create time generated messages on homepage

--- src/main/java/com/aunraza/welcomepage/WelcomeController.java
+++ src/main/java/com/aunraza/welcomepage/WelcomeController.java
@@ -4,10 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
 @RestController
 public class WelcomeController {
     @RequestMapping("/")
     String welcome() {
-        return "<h1>Welcome to COMP367</h1>";
+        int hour = LocalTime.now().getHour();
+        if (hour >= 6 && hour < 12) {
+            return "<h1>Good Morning, Aun, Welcome to COMP367</h1>";
+        }
+        else {
+            return "<h1>Good Afternoon, Aun, Welcome to COMP367</h1>";
+        }
     }
 }
